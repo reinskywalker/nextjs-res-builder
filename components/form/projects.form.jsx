6e466d3c91a216879c1f8f56d816e9184bf0eb9b
Reynaldi <andre.reynaldi@ai.astra.co.id>
2023-12-14 11:43:19 +0700

    utilize map and high order func

--- components/form/projects.form.jsx
+++ components/form/projects.form.jsx
@@ -1,23 +1,23 @@
-import FormButton from "./button.form";
 import React, { useContext } from "react";
 import { ResumeContext } from "../../pages/builder";
+import FormButton from "./button.form";
 
 const Projects = () => {
   const { resumeData, setResumeData } = useContext(ResumeContext);
 
   const handleProjects = (e, index) => {
     const newProjects = [...resumeData.projects];
-    newProjects[index][e.target.name] = e.target.value;
+    newProjects[index] = { ...newProjects[index], [e.target.name]: e.target.value };
     setResumeData({ ...resumeData, projects: newProjects });
   };
 
-  const addProjects = () => {
+  const addProject = () => {
     setResumeData({
       ...resumeData,
       projects: [
         ...resumeData.projects,
         {
-          title: "",
+          name: "",
           link: "",
           description: "",
           keyAchievements: "",
@@ -28,10 +28,9 @@ const Projects = () => {
     });
   };
 
-  const removeProjects = (index) => {
+  const removeProject = (index) => {
     const newProjects = [...resumeData.projects];
-    newProjects[index] = newProjects[newProjects.length - 1];
-    newProjects.pop();
+    newProjects.splice(index, 1);
     setResumeData({ ...resumeData, projects: newProjects });
   };
 
@@ -40,64 +39,33 @@ const Projects = () => {
       <h2 className="input-title">Projects</h2>
       {resumeData.projects.map((project, index) => (
         <div key={index} className="f-col">
-          <input
-            type="text"
-            placeholder="Project Name"
-            name="name"
-            className="w-full other-input"
-            value={project.name}
-            onChange={(e) => handleProjects(e, index)}
-          />
-          <input
-            type="text"
-            placeholder="Link"
-            name="link"
-            className="w-full other-input"
-            value={project.link}
-            onChange={(e) => handleProjects(e, index)}
-          />
-          <textarea
-            type="text"
-            placeholder="Description"
-            name="description"
-            className="w-full other-input h-32"
-            value={project.description}
-            maxLength="250"
-            onChange={(e) => handleProjects(e, index)}
-          />
-          <textarea
-            type="text"
-            placeholder="Key Achievements"
-            name="keyAchievements"
-            className="w-full other-input h-40"
-            value={project.keyAchievements}
-            onChange={(e) => handleProjects(e, index)}
-          />
+          {["name", "link", "description", "keyAchievements"].map((field) => (
+            <input
+              key={field}
+              type="text"
+              placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+              name={field}
+              className={`w-full other-input ${field === "description" ? "h-32" : "h-40"}`}
+              value={project[field]}
+              onChange={(e) => handleProjects(e, index)}
+            />
+          ))}
           <div className="flex-wrap-gap-2">
-            <input
-              type="date"
-              placeholder="Start Year"
-              name="startYear"
-              className="other-input"
-              value={project.startYear}
-              onChange={(e) => handleProjects(e, index)}
-            />
-            <input
-              type="date"
-              placeholder="End Year"
-              name="endYear"
-              className="other-input"
-              value={project.endYear}
-              onChange={(e) => handleProjects(e, index)}
-            />
+            {["startYear", "endYear"].map((field) => (
+              <input
+                key={field}
+                type="date"
+                placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                name={field}
+                className="other-input"
+                value={project[field]}
+                onChange={(e) => handleProjects(e, index)}
+              />
+            ))}
           </div>
         </div>
       ))}
-      <FormButton
-        size={resumeData.projects.length}
-        add={addProjects}
-        remove={removeProjects}
-      />
+      <FormButton size={resumeData.projects.length} add={addProject} remove={removeProject} />
     </div>
   );
 };
